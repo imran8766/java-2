6580c6639e011e3a46f8123fc36d1e8cc958987b
MD IMRAN SHEIKH <166485094+imran8766@users.noreply.github.com>
2024-07-28 12:26:04 +0600

    gu

---
+++ array.js
@@ -0,0 +1,12 @@
+var number = [20, 22, 23, 24, 25]; // array korar neyom eta
+console.log(number);
+console.log(number[0]); // etar dara positon likhe dela setai output hoi.position start hoi 0 theke
+ var myage = number[4];// array theke kono input nea dekhate hole ei process
+ console.log(myage);
+
+
+ var nw = [31, 32, 33, 34];
+nw[2] = 39;// arry er moddher input change korar neyom
+console.log(nw);
+var position = nw.indexOf(32);
+console.log(position);
