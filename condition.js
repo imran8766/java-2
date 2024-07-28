4f17d63651fb9e428bdf2aef87972d26f85978ef
MD IMRAN SHEIKH <166485094+imran8766@users.noreply.github.com>
2024-07-28 11:51:57 +0600

    dfd

---
+++ condition.js
@@ -0,0 +1,25 @@
+
+
+var cod = true;
+var codg = 1800;
+
+if(cod == true && codg > 1900){ //duita condition e puron hoa lagbe 
+console.log("you can proceed ");
+}
+else{
+console.log("work hard");
+}
+
+//sesh
+
+
+
+var fod = false;
+var fod2 = 2000;
+
+if(fod == false || fod2 > 19000){ // je kono akta condition puron holei hobe
+console.log("you can proceed ");
+}
+else{
+console.log("work hard");
+}
