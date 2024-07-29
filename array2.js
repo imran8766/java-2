f5f9800cc1adc5a686e2eff6de90e9f8b3d79538
MD IMRAN SHEIKH <166485094+imran8766@users.noreply.github.com>
2024-07-29 11:04:15 +0600

    ygyg

--- array2.js
+++ array2.js
@@ -10,3 +10,19 @@ console.log(nw.length); //array er moddhe koita element ber korar neyom
 
 nw.pop(); // array er moddhe sesh er ta ber hoa jabe
 console.log(nw);
+
+console.log(nw.length);
+
+// new part start here
+
+var part = [87, 88, 89, 90, 91, 92];
+console.log(part);
+var now = part.slice(3); // array er majhe kono jaiga theke start koret
+console.log(now);
+
+part.unshift(86); // array er first e add kora
+part.unshift(85);
+console.log(part);
+part.shift(); //array er first e remove kora
+console.log(part);
+
