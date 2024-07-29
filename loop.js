a9b4882998ce9ed71c3b5ed7d4e4b3c76c721488
MD IMRAN SHEIKH <166485094+imran8766@users.noreply.github.com>
2024-07-29 12:27:26 +0600

    ygfygdfs

---
+++ loop.js
@@ -0,0 +1,21 @@
+var num = 3;
+while(num < 10)
+    {
+
+console.log(num);
+num = num + 1;
+}
+
+// for loop
+for(var i = 2; i < 15; i++){
+console.log(i);
+
+}
+
+// array for loop
+
+var der = [20, 22, 24, 26, 28, 30, 32];
+for(var i = 0; i < der.length; i++){
+ var daa = der[i];
+    console.log(daa);
+}
