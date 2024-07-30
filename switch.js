b9890f3671bc2c15abd82a884d4c65eeefe325a5
MD IMRAN SHEIKH <166485094+imran8766@users.noreply.github.com>
2024-07-30 13:00:49 +0600

    uyfysf

---
+++ switch.js
@@ -0,0 +1,19 @@
+num = 25;
+switch(num){
+        case 10:
+            console.log('i am 10');
+            break;
+        case 20:
+            console.log('i am 20');  
+            break;
+        case 30:
+            console.log('i am 30');   
+            break;
+        case 40:
+            console.log('i am 40');     
+            break;
+        default:
+            console.log('i dont know');
+
+
+}
